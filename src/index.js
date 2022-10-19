da17aa20d537d5199ac10075209293e69011f352
Thiago Puntar <thiagopuntar@gmail.com>
2022-10-19 20:18:25 -0300

    implement initial crud

--- src/index.js
+++ src/index.js
@@ -1,25 +1,51 @@
-const express = require('express');
-const fs = require('fs').promises;
-const path = require('path');
+const express = require("express");
+const { Schema, model, connect } = require("mongoose");
+
+const CREATED = 201;
+const ACCEPTED = 202;
+
+const recordSchema = new Schema({
+  date: Date,
+  description: String,
+  value: Number,
+});
+
+const Record = model("Record", recordSchema);
 
 const app = express();
 const port = 3005;
 
-app.get('/api', (req, res) => {
-  const filePath = path.join(__dirname, 'data/records.json');
-  fs.readFile(filePath, 'utf-8').then((file) => JSON.parse(file)).then((jsonData) => {
-    res.json(jsonData);
+async function main() {
+  await connect("mongodb://localhost:27017/test");
+  app.use(express.json());
+
+  app.get("/api", async (req, res) => {
+    const records = await Record.find();
+    res.json(records);
   });
-});
 
-app.post('/api', (req, res) => {
-  res.send('hello world! post');
-});
+  app.post("/api", async (req, res) => {
+    const record = await Record.create(req.body);
+    res.status(CREATED).json(record);
+  });
 
-app.delete('/api', (req, res) => {});
+  app.delete("/api/:id", async (req, res) => {
+    const { id } = req.params;
+    const deletedRecord = await Record.findByIdAndDelete(id);
+    res.status(ACCEPTED).json(deletedRecord);
+  });
 
-app.put('/', (req, res) => {});
+  app.put("/api/:id", async (req, res) => {
+    const { id } = req.params;
+    const updatedRecord = await Record.findByIdAndUpdate(id, req.body, {
+      returnDocument: "after",
+    });
+    res.status(ACCEPTED).json(updatedRecord);
+  });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+  });
+}
+
+main().catch((err) => console.log(err));
