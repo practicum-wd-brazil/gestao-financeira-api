508276c7b44e11b2f8813ac9da6015184190dda8
Thiago Puntar <thiagopuntar@gmail.com>
2022-10-19 20:37:40 -0300

    implement controllers

---
+++ src/controllers/records.controller.js
@@ -0,0 +1,43 @@
+const { Schema, model } = require("mongoose");
+
+const CREATED = 201;
+const ACCEPTED = 202;
+
+const recordSchema = new Schema({
+  date: Date,
+  description: String,
+  value: Number,
+});
+
+const Record = model("Record", recordSchema);
+
+const getRecords = async (req, res) => {
+  const records = await Record.find();
+  res.json(records);
+};
+
+const createRecord = async (req, res) => {
+  const record = await Record.create(req.body);
+  res.status(CREATED).json(record);
+};
+
+const deleteRecord = async (req, res) => {
+  const { id } = req.params;
+  const deletedRecord = await Record.findByIdAndDelete(id);
+  res.status(ACCEPTED).json(deletedRecord);
+};
+
+const updateRecord = async (req, res) => {
+  const { id } = req.params;
+  const updatedRecord = await Record.findByIdAndUpdate(id, req.body, {
+    returnDocument: "after",
+  });
+  res.status(ACCEPTED).json(updatedRecord);
+};
+
+module.exports = {
+  getRecords,
+  createRecord,
+  deleteRecord,
+  updateRecord,
+};
--- src/index.js
+++ src/index.js
@@ -1,47 +1,27 @@
+require("dotenv").config();
 const express = require("express");
-const { Schema, model, connect } = require("mongoose");
-
-const CREATED = 201;
-const ACCEPTED = 202;
-
-const recordSchema = new Schema({
-  date: Date,
-  description: String,
-  value: Number,
-});
-
-const Record = model("Record", recordSchema);
+const { connect } = require("mongoose");
+const {
+  getRecords,
+  createRecord,
+  deleteRecord,
+  updateRecord,
+} = require("./controllers/records.controller");
 
 const app = express();
 const port = 3005;
 
 async function main() {
-  await connect("mongodb://localhost:27017/test");
+  await connect(process.env.MONGODB_URL);
   app.use(express.json());
 
-  app.get("/api", async (req, res) => {
-    const records = await Record.find();
-    res.json(records);
-  });
+  app.get("/api", getRecords);
 
-  app.post("/api", async (req, res) => {
-    const record = await Record.create(req.body);
-    res.status(CREATED).json(record);
-  });
+  app.post("/api", createRecord);
 
-  app.delete("/api/:id", async (req, res) => {
-    const { id } = req.params;
-    const deletedRecord = await Record.findByIdAndDelete(id);
-    res.status(ACCEPTED).json(deletedRecord);
-  });
+  app.delete("/api/:id", deleteRecord);
 
-  app.put("/api/:id", async (req, res) => {
-    const { id } = req.params;
-    const updatedRecord = await Record.findByIdAndUpdate(id, req.body, {
-      returnDocument: "after",
-    });
-    res.status(ACCEPTED).json(updatedRecord);
-  });
+  app.put("/api/:id", updateRecord);
 
   app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
